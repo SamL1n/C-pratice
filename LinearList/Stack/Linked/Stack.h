53a753c20ad55b8c55cb6adefb0960c3ed2620d3
sam <lingourui@gmail.com>
2022-05-02 17:35:10 +0800

    add Stack without head node (link)

---
+++ LinearList/Stack/Linked/Stack.h
@@ -0,0 +1,70 @@
+#include <stdlib.h>
+#include <stdbool.h>
+
+//不带头结点的链栈
+typedef struct
+{
+    int value;
+    LNode *next;
+} LNode, *StackList;
+
+void InitStack(StackList s)
+{
+    s = NULL;
+}
+
+bool Push(StackList s, int value)
+{
+    LNode *node = (LNode *)malloc(sizeof(LNode));
+    if (s == NULL)
+    {
+        s = node;
+        s->next = NULL;
+        s->value = value;
+    }
+    else
+    {
+        node->next = s;
+        node->value = value;
+        s = node;
+    }
+    return true;
+}
+
+bool Pop(StackList s)
+{
+    if (s == NULL)
+    {
+        return false;
+    }
+    LNode *p = s;
+    s = s->next;
+    free(p);
+    return true;
+}
+
+bool Get(StackList s, int *n)
+{
+    if (s == NULL)
+    {
+        return false;
+    }
+    else
+    {
+        *n = s->value;
+        return true;
+    }
+}
+
+bool IsEmpty(StackList s)
+{
+    return s == NULL;
+}
+
+void DestroyStack(StackList s)
+{
+    while (s)
+    {
+        Pop(s);
+    }
+}
